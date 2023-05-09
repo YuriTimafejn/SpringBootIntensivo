74f379dd6c215569b9c1f0f394cd69261d7f2f43
Yuri Timafejn <timafejn@gmail.com>
2023-05-09 20:21:25 -0300

    Project created

---
+++ src/test/java/com/timafejn/controle_filmes/ControleFilmesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timafejn.controle_filmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleFilmesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
