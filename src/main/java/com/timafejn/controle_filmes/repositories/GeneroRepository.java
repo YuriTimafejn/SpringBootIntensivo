94665245e4b002b192743608c263efdd25c3e1df
Yuri Timafejn <timafejn@gmail.com>
2023-06-24 21:51:37 -0300

    Created method FindByGenero

--- src/main/java/com/timafejn/controle_filmes/repositories/GeneroRepository.java
+++ src/main/java/com/timafejn/controle_filmes/repositories/GeneroRepository.java
@@ -4,4 +4,5 @@ import com.timafejn.controle_filmes.entities.Genero;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GeneroRepository extends JpaRepository<Genero, Long> {
+    Genero findByGenero(String genero);
 }
--- src/main/java/com/timafejn/controle_filmes/servicies/GeneroService.java
+++ src/main/java/com/timafejn/controle_filmes/servicies/GeneroService.java
@@ -5,6 +5,7 @@ import com.timafejn.controle_filmes.entities.Genero;
 import com.timafejn.controle_filmes.repositories.GeneroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,8 +14,14 @@ public class GeneroService {
     @Autowired
     private GeneroRepository generoRepository;
 
+    @Transactional(readOnly = true)
     public List<GeneroDTO> findAlL(){
         List<Genero> generos = generoRepository.findAll();
         return generos.stream().map(GeneroDTO::new).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public GeneroDTO findByGenero (String genero) {
+        return new GeneroDTO(generoRepository.findByGenero(genero));
     }
 }
